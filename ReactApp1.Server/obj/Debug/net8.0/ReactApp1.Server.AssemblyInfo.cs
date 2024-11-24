01cc84cb3d4de76bb7170dccf841944128e4ada7
Memz-Dev <146094234+Memz-Dev@users.noreply.github.com>
2024-11-24 22:30:17 +0100

    a

--- ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b59787ab4fb338d89d0a9a67d7cc486a5b8e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9f5217d0e0414c4fae05591f364391364c759e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
