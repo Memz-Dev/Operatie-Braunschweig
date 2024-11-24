31cd18a02f46764b7110debada500f76e4faf2e6
Memz-Dev <146094234+Memz-Dev@users.noreply.github.com>
2024-11-24 22:17:41 +0100

    test

--- ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49cf2a1c0d40db45adddd43e79ba2daabd269f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00125175d1892dd7c98fbbfdb5e335c1e24d7fb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ReactApp1.Server/obj/Release/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Release/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1523c96fb88bec2838f3366a7acc2ceefb9f3ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00125175d1892dd7c98fbbfdb5e335c1e24d7fb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
