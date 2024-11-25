0dfbf37ca147bc5d1e99d3a60fa67d071ea19358
samuel2597 <164479057+samuel2597@users.noreply.github.com>
2024-11-25 15:34:44 +0100

    adres die 2x voorkwam weg gehaald.

--- ReactApp1.Server/Classes/HuurdersAccount.cs
+++ ReactApp1.Server/Classes/HuurdersAccount.cs
@@ -13,16 +13,11 @@ namespace ReactApp1.Server.Classes
         [EmailAddress(ErrorMessage = "Ongeldig e-mailadres.")]
         public string EmailAdres { get; set; }
 
-<<<<<<< HEAD
 
-        [Required(ErrorMessage = "Adres is verplicht")]
-        public string Adres { get; set; }
 
-=======
         [Required(ErrorMessage = "Adres is verplicht.")]
         [RegularExpression(@"^[a-zA-Z0-9À-ÿ.,' -]+$", ErrorMessage = "Adres mag alleen letters, cijfers, spaties en standaard leestekens bevatten.")]
         public string Adres { get; set; }
->>>>>>> ac5c10136917391e4906d20c28e12315a35120a7
 
         [Phone(ErrorMessage = "Ongeldig telefoonnummer.")]
         public string Telefoonnummer { get; set; }
