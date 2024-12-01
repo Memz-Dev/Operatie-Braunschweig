f5c7815a0e5058f613ea91888c7f46b6b11e5386
Sjoerd Verhagen <23079924@student.hhs.nl>
2024-12-01 11:36:46 +0100

    Commit before pull

--- ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cb7786352cb4ea9ef3c87b8d095d84e066413b")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823d7350536974e4134706a40766182048227b13")]
->>>>>>> d2cacca97c1fee6adfcdfa5dc540033806491574
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed63671349fd1ed0c912c6df888f46d5ac85c9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
